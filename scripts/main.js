e58f09e82c8eccd86127117c329d10ca06f97b3b
Matheus-Pombeiro <pombeiromatheus@gmail.com>
2023-10-12 14:48:35 -0300

    Add: create new script and HTML references in this document

---
+++ scripts/main.js
@@ -0,0 +1,5 @@
+// Get elements from HTML
+const from = document.querySelector("[data-select-from]");      // From
+const inFrom = document.querySelector("[data-inFrom]");
+const to = document.querySelector("[data-select-to]");          // To
+const inTo = document.querySelector("[data-inTo]");
