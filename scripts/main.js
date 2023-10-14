554bd35043bf811c7c3186162d5c58203f46879d
Matheus-Pombeiro <pombeiromatheus@gmail.com>
2023-10-14 09:50:13 -0300

    Add: references of the HTML document

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,6 @@
 // Get elements from HTML
+const form = document.querySelector("form");                    // Form
+const submit = document.querySelector("[data-submit]");         // Submit
 const from = document.querySelector("[data-select-from]");      // From
 const inFrom = document.querySelector("[data-inFrom]");
 const to = document.querySelector("[data-select-to]");          // To
