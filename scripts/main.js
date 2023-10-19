d984eb79a89fefcf4115a64fbd788224b6741735
Matheus-Pombeiro <pombeiromatheus@gmail.com>
2023-10-19 20:45:40 -0300

    Remove: a unused reference

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,6 @@ import openModal from "./modal.js";
 
 // Get elements from HTML
 const form = document.querySelector("form");                    // Form
-const submit = document.querySelector("[data-submit]");         // Submit
 const from = document.querySelector("[data-select-from]");      // From
 const inFrom = document.querySelector("[data-inFrom]");
 const to = document.querySelector("[data-select-to]");          // To
