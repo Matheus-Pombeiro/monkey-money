98cfa1bb4c0600e81443fe8987afb46accde0d08
Matheus-Pombeiro <pombeiromatheus@gmail.com>
2023-10-19 21:08:12 -0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -12,7 +12,7 @@ const theme = document.querySelector("[data-theme-btn]");       // Theme button
 const modal = document.querySelector("[data-modal]");           // Modal (pop-up)
 const closeModal = document.querySelector("[data-modal-close"); // Close modal button
 
-themeChange(theme);                 // Call the change theme function+
+themeChange(theme);                 // Call the change theme function
 openModal(modal, closeModal);       // Open and close the modal
 
 form.addEventListener("submit", (e) => {
