55fec5a820615d4901b9396d20c9eeb8075210ab
Matheus-Pombeiro <pombeiromatheus@gmail.com>
2023-10-21 10:01:48 -0300

    Update theme.js

--- scripts/theme.js
+++ scripts/theme.js
@@ -3,7 +3,13 @@ export default function themeChange(theme) {
     theme.addEventListener("click", () => {
 
         document.body.classList.toggle("dark");
-        document.body.classList.contains("dark") ? theme.src = "../assets/dark-button.png" : theme.src = "../assets/light-button.png";
+        // document.body.classList.contains("dark") ? theme.src = "../assets/dark-button.png" : theme.src = "../assets/light-button.png";
+
+        if (document.body.classList.contains("dark")) {
+            theme.src = "../assets/dark-button.png";
+        } else {
+            theme.src = "../assets/light-button.png";
+        }
 
     });
 
