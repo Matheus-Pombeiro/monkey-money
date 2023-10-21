6f13760421355fc6867c2437dbe8a7932374ad99
Matheus-Pombeiro <pombeiromatheus@gmail.com>
2023-10-21 10:12:55 -0300

    Fix: code refactoring

--- scripts/theme.js
+++ scripts/theme.js
@@ -3,13 +3,7 @@ export default function themeChange(theme) {
     theme.addEventListener("click", () => {
 
         document.body.classList.toggle("dark");
-        // document.body.classList.contains("dark") ? theme.src = "../assets/dark-button.png" : theme.src = "../assets/light-button.png";
-
-        if (document.body.classList.contains("dark")) {
-            theme.src = "../assets/dark-button.png";
-        } else {
-            theme.src = "../assets/light-button.png";
-        }
+        document.body.classList.contains("dark") ? theme.src = "./assets/dark-button.png" : theme.src = "./assets/light-button.png";
 
     });
 
